e7d379fcdcdf040a730def0ffc8c9b9337bc86c6
tom-lev <tomer9tomer@gmail.com>
2025-05-05 22:02:08 +0300

    initail commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+let currentInput = '';
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+
+    if (value === 'C') {
+      currentInput = '';
+      display.value = '';
+    } else if (value === '=') {
+      try {
+        currentInput = eval(currentInput).toString();
+        display.value = currentInput;
+      } catch {
+        display.value = 'Error';
+        currentInput = '';
+      }
+    } else {
+      currentInput += value;
+      display.value = currentInput;
+    }
+  });
+});
